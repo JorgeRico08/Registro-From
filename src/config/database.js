1c515185f4e1236813c7f843fd599f0374498321
JorgeRico08 <ricoarvizujorge2002@gmail.com>
2022-11-12 22:54:21 -0600

    Cambios finales

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/registro-Alumnos', {
-    useNewUrlParser: true
-})
-    .then(db => console.log('Db is connected'))
-    .catch(err => console.log(err));
+// mongoose.connect('mongodb://localhost/registro-Alumnos', {
+//     useNewUrlParser: true
+// })
+//     .then(db => console.log('Db is connected'))
+//     .catch(err => console.log(err));
+
+mongoose.connect('mongodb+srv://Jorge:Jorge01@cluster0.tbdkk.mongodb.net/Registro-form', {
+        useNewUrlParser: true
+    })
+        .then(db => console.log('Db is connected'))
+        .catch(err => console.log(err));
